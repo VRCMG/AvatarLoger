e1426ec4965259f62199a9c2731772b2629e66da
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-04-19 08:09:18 +0200

    Fixed Config

--- Config.cs
+++ Config.cs
@@ -4,9 +4,9 @@
     {
         public string PublicWebhook { get; set; }
         public string PrivateWebhook { get; set; }
-        public static string BotName => null;
-        public static string AvatarURL => null;
+        public string BotName { get; set; }
+        public string AvatarURL { get; set; }
         public bool CanPostFriendsAvatar { get; set; }
-        public static bool CanPostSelfAvatar => false;
+        public bool CanPostSelfAvatar { get; set; }
     }
 }
