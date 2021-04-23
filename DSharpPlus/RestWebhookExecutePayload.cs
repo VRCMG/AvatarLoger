df37cb749bf3e1930d18f8ccbea75533cabff81e
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2021-04-23 14:10:43 +0200

    Removed unused usings

--- DSharpPlus/RestWebhookExecutePayload.cs
+++ DSharpPlus/RestWebhookExecutePayload.cs
@@ -1,10 +1,6 @@
 ﻿using DSharpPlus.Entities;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSharpPlus
 {
